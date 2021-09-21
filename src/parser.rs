2ea6129c08877f912cd095651fbfdbf2e02be8c3
togatoga <togasakitogatoga@gmail.com>
2021-09-21 23:13:08 +0900

    Add comments

--- src/parser.rs
+++ src/parser.rs
@@ -13,15 +13,22 @@ impl ParserError {
     }
 }
 pub struct Parser {
+    // `Lexer`で`tokenize`した`Token`一覧
     tokens: Vec<Token>,
+    // `tokens`の先頭
     index: usize,
 }
 
 impl Parser {
+    /// `Token`の一覧を受け取り`Parser`を返す。
     pub fn new(tokens: Vec<Token>) -> Parser {
         Parser { tokens, index: 0 }
     }
+
+    /// `Array`のパースを行う。
+    ///  [1, 2, 3, null, "string"]
     fn parse_array(&mut self) -> Result<Value, ParserError> {
+        // 先頭は必ず [
         let token = self.peek_expect()?;
         if *token != Token::LeftBracket {
             return Err(ParserError::new(&format!(
@@ -29,24 +36,29 @@ impl Parser {
                 token
             )));
         }
+        // [ を読み飛ばす
         self.next_expect()?;
 
         let mut array = vec![];
         let token = self.peek_expect()?;
-
+        // ] なら空配列を返す
         if *token == Token::RightBracket {
             return Ok(Value::Array(array));
         }
 
         loop {
+            // 残りの`Value`をパースする
             let value = self.parse()?;
             array.push(value);
-            let token = self.next_expect()?;
 
+            // `Array`が終端もしくは次の要素(`Value`)があるかを確認
+            let token = self.next_expect()?;
             match token {
+                // ] は`Array`の終端
                 Token::RightBracket => {
                     return Ok(Value::Array(array));
                 }
+                // , なら次の要素(`Value`)をパースする
                 Token::Comma => {
                     continue;
                 }
@@ -59,7 +71,14 @@ impl Parser {
             }
         }
     }
+
+    /// `Object`のパースを行う。
+    /// {
+    ///   "key1": 12345,
+    ///   "key2": 6789
+    /// }
     fn parse_object(&mut self) -> Result<Value, ParserError> {
+        // 先頭は必ず {
         let token = self.peek_expect()?;
         if *token != Token::LeftBrace {
             return Err(ParserError::new(&format!(
@@ -67,37 +86,50 @@ impl Parser {
                 token
             )));
         }
+        // { を読み飛ばす
         self.next_expect()?;
 
         let mut object = std::collections::BTreeMap::new();
 
+        // } なら空の`Object`を返す
         if *self.peek_expect()? == Token::RightBrace {
             return Ok(Value::Object(object));
         }
 
         loop {
-            // "togatoga" : [1, 2, 3, 4]
+            // 2文字分の`Token`を読み出す
             let token1 = self.next_expect()?.clone();
-
             let token2 = self.next_expect()?;
 
             match (token1, token2) {
+                // token1とtoken2はそれぞれ"key"(`Token::String`)と:(`Token::Colon`)であることを想定してる
+                // e.g. "key" : 12345
+                // "key" (`Token::String`)
+                // : (`Token::Colon`)
                 (Token::String(key), Token::Colon) => {
+                    // 残りの`Value`(12345)をパースする。
                     object.insert(key, self.parse()?);
                 }
+                // それ以外はエラー
                 _ => {
                     return Err(ParserError::new(
                         "error: a pair (key(string) and : token) token is expected",
                     ));
                 }
             }
+            // `Object`が終端かもしくは次の要素(key-value)があるか
             let token3 = self.next_expect()?;
             match token3 {
+                // } `Object`の終端だったら`object`を返す
                 Token::RightBrace => {
                     return Ok(Value::Object(object));
                 }
+                // , なら次の要素(key-value)のパースする
+                // {
+                //    "key1": 12345,
+                //    "key2": 6789
+                // }
                 Token::Comma => {
-                    // next token
                     continue;
                 }
                 _ => {
@@ -110,10 +142,13 @@ impl Parser {
         }
     }
 
+    /// `Token`を評価して`Value`に変換する。この関数は再帰的に呼び出される。
     pub fn parse(&mut self) -> Result<Value, ParserError> {
         let token = self.peek_expect()?.clone();
         let value = match token {
+            // { はObjectの開始文字
             Token::LeftBrace => self.parse_object(),
+            // [ はArrayの開始文字
             Token::LeftBracket => self.parse_array(),
             Token::String(s) => {
                 self.next_expect()?;
@@ -140,18 +175,24 @@ impl Parser {
         };
         value
     }
-
+    /// 先頭の`Token`を返す。
     fn peek(&self) -> Option<&Token> {
         self.tokens.get(self.index)
     }
+
+    /// 先頭の`Token`を返す。(先頭に`Token`があることを想定してる)
     fn peek_expect(&self) -> Result<&Token, ParserError> {
         self.peek()
             .ok_or_else(|| ParserError::new("error: a token isn't peekable"))
     }
+
+    /// 先頭の`Token`を返して、1トークン進める。
     fn next(&mut self) -> Option<&Token> {
         self.index += 1;
         self.tokens.get(self.index - 1)
     }
+
+    /// 先頭の`Token`を返して、1トークン進める。(先頭に`Token`があることを想定してる)
     fn next_expect(&mut self) -> Result<&Token, ParserError> {
         self.next()
             .ok_or_else(|| ParserError::new("error: a token isn't peekable"))
